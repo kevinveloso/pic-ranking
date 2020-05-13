4abfd202891513e58f5e3c2793b2cf158d9b247d
Kevin Veloso <kevinveloso@gmail.com>
2020-05-13 19:00:29 -0300

    First Commit

---
+++ src/main/java/com/picranking/picranking/PicrankingApplication.java
@@ -0,0 +1,13 @@
+package com.picranking.picranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicrankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicrankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picranking/picranking/PicrankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picranking.picranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicrankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
